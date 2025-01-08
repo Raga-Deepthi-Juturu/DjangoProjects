d3cf3583935812656dfd9149957ae37c6779798f
JuturuRagaDeepthi <143383842+JuturuRagaDeepthi@users.noreply.github.com>
2025-01-09 05:08:08 +0530

    Intial commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def startingPage(request):
+    return render(request,'blogs/index.html')
+
+def posts(request):
+    return HttpResponse("hii, this page contains all posts")
+
+def postDetails(request, slug):
+    return HttpResponse(f"Details for post: {slug}")
