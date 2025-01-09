d584c853fa42e69ff8c918e338d69bec2d42be65
JuturuRagaDeepthi <143383842+JuturuRagaDeepthi@users.noreply.github.com>
2025-01-09 05:54:34 +0530

    added html and css files

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def startingPage(request):
-    return render(request,'blogs/index.html')
+    return render(request,'blog/index.html')
 
 def posts(request):
     return HttpResponse("hii, this page contains all posts")
